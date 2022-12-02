6aa35cc50eb5169d75f67291a826488d05d41d78
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-12-02 13:09:44 +0000

    Complete deparment service

--- department-service/src/main/java/com/rbanos/department/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/rbanos/department/DepartmentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.rbanos.departmentservice;
+package com.rbanos.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
